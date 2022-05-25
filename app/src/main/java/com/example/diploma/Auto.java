8dbea5fac4c5be9dcc6649c0f24578971fbcd6e0
Александра Смагина <keepsa02@outlook.com>
2022-05-25 16:42:00 +0300

    updates

--- app/src/main/java/com/example/diploma/Auto.java
+++ app/src/main/java/com/example/diploma/Auto.java
@@ -3,6 +3,7 @@ package com.example.diploma;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -10,41 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Auto extends AppCompatActivity {
+    EditText login, password;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.auto);
 
         //да что бл не так
+
+
         /*
-        if (findViewById(R.id.editTextTextPersonName.getText().toString().equals(""))) {
-            // Здесь код, если EditText пуст
-            Toast.makeText(this, "Введите логин!", Toast.LENGTH_LONG);
-        }
-        else
-        {
-        // если есть текст, то здесь другой код
-            return;
-        }
-        */
-
-
-
-
-
-/*
-
-        EditText login, password;
-        boolean isNullOrEmmpty;
-        String str;
-
         login = (EditText)findViewById(R.id.editTextTextPersonName);
         password = (EditText)findViewById(R.id.editTextTextPassword);
         if (isNullOrEmpty(login) || isNullOrEmpty(password)){
             Toast.makeText(this, "Введите логин и пароль!", Toast.LENGTH_LONG);
         }
-*/
 
+*/
 
 /*
         private boolean vallidation() {
@@ -68,8 +52,25 @@ public class Auto extends AppCompatActivity {
     }
 */
     public void ClickMain(View view) {
-        Intent intent = new Intent(Auto.this, MainActivity.class);
-        startActivity(intent);
+
+        login = (EditText)findViewById(R.id.editTextTextPersonName);
+        password = (EditText)findViewById(R.id.editTextTextPassword);
+        if (login.getText().toString().trim().equals("")) {
+            // Здесь код, если EditText пуст
+            Toast.makeText(this, "Введите логин!", Toast.LENGTH_LONG);
+        }
+        else if (password.getText().toString().trim().equals(""))
+        {
+            Toast.makeText(this, "Введите пароль!", Toast.LENGTH_LONG);
+        }
+        else
+        {
+            // если есть текст, то здесь другой код
+            Intent intent = new Intent(Auto.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
     }
 
 }
